88d6a4457f42dbe005fbc4d4b130d6b950c2ef6d
ian <ian.ribeiro4@gmail.com>
2026-08-14 18:23:11 -0300

    update

--- trim_private.sh
+++ trim_private.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Go to chezmoi source directory
-cd "$(chezmoi source-path)" || exit 1
+cd "$(chezmoi source-path)"/dot_config || exit 1
 
 # Rename private_* files and directories recursively
 find . -depth -name "private_*" | while read -r file; do
