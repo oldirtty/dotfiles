81f5a900b32ebaa2bcbe2b5564451a312d552c33
ian <ian.ribeiro4@gmail.com>
2026-09-06 12:36:03 -0300

    improve reload noctalia script

--- dot_bin/executable_reload-noctalia.sh
+++ dot_bin/executable_reload-noctalia.sh
@@ -2,4 +2,16 @@
 
 pkill -x noctalia
 sleep 1
-noctalia -d
+
+LOCAL="$HOME/.local/bin/noctalia"
+GLOBAL="/usr/bin/noctalia"
+
+if [ -f "$LOCAL" ]; then
+  echo "Running $LOCAL -d"
+  "$LOCAL" -d
+elif [ -f "$GLOBAL" ]; then
+  echo "Running $GLOBAL -d"
+  "$GLOBAL" -d
+else
+  echo "Noctalia not found!"
+fi
